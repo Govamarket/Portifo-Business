614157e3a4034e960fde00b1ccdbe6e9b8a28292
Gova <govaway@gmail.com>
2023-01-08 21:14:37 +0100

    Intial commit

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ const navSlide = () => {
 let number = document.getElementById('number');
 let counter = 0;
 setInterval(() => {
-    if (condition) {
-        
-    }
+    
     counter += 1;
     number.innerHTML = counter + "%"
 }, 1000);
