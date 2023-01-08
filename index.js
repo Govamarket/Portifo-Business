4e17f875a1a1a0b22330f25aac524ea3fdf8ddaa
Gova <govaway@gmail.com>
2023-01-08 21:51:32 +0100

    Intial commit

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    // the toggle navigation
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        // Animate links
-        navLinks.forEach((link, index) => {
-           if (link.style.animation) {
-                link.style.animation = '';
-           }else{
-            link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-           }
-        
-        })
-        // Burger animation
-        burger.classList.toggle('toggle');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  // the toggle navigation
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    // Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinksFade 0.5s ease forwards ${
+          index / 7 + 1.5
+        }s`;
+      }
     });
-
+    // Burger animation
+    burger.classList.toggle("toggle");
+  });
 };
 
-
-let number = document.getElementById('number');
+let number = document.querySelector("#number");
 let counter = 0;
 setInterval(() => {
-    
-    counter += 1;
-    number.innerHTML = counter + "%"
+
+    if (counter == 30) {
+        clearInterval();
+    }else{
+        counter += 1;
+        number.innerHTML = counter + "%";
+    }
+}, 1000);
+
+let cssStyle = document.querySelector("#css");
+let time = 0;
+setInterval(() => {
+    if (time == 30) {
+        clearInterval();
+    }else{
+        time += 1;
+        css.innerHTML = counter + "%";
+    }
+}, 1000);
+
+let bootstrap = document.querySelector("#strap");
+let count = 0;
+setInterval(() => {
+    if (count == 30) {
+        clearInterval();
+    }else{
+        count += 1;
+        strap.innerHTML = counter + "%";
+    }
+}, 1000);
+
+let javaScript = document.querySelector("#script");
+let limit = 0;
+setInterval(() => {
+    if (limit == 30) {
+       clearInterval(); 
+    }else{
+        limit += 1;
+        script.innerHTML = counter + "%";
+    }
 }, 1000);
 navSlide();
-
-
-
-
