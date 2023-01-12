4fa4f271a47be954f3dfe64d6d53a46212f3444e
Gova <govaway@gmail.com>
2023-01-12 19:21:19 +0100

    Intial commit

--- index.js
+++ index.js
@@ -23,45 +23,86 @@ const navSlide = () => {
 let number = document.querySelector("#number");
 let counter = 0;
 setInterval(() => {
-
-    if (counter == 30) {
-        clearInterval();
-    }else{
-        counter += 1;
-        number.innerHTML = counter + "%";
-    }
+  if (counter == 30) {
+    clearInterval();
+  } else {
+    counter += 1;
+    number.innerHTML = counter + "%";
+  }
 }, 1000);
 
 let cssStyle = document.querySelector("#css");
 let time = 0;
 setInterval(() => {
-    if (time == 30) {
-        clearInterval();
-    }else{
-        time += 1;
-        css.innerHTML = counter + "%";
-    }
+  if (time == 30) {
+    clearInterval();
+  } else {
+    time += 1;
+    css.innerHTML = counter + "%";
+  }
 }, 1000);
 
 let bootstrap = document.querySelector("#strap");
 let count = 0;
 setInterval(() => {
-    if (count == 30) {
-        clearInterval();
-    }else{
-        count += 1;
-        strap.innerHTML = counter + "%";
-    }
+  if (count == 30) {
+    clearInterval();
+  } else {
+    count += 1;
+    strap.innerHTML = counter + "%";
+  }
 }, 1000);
 
 let javaScript = document.querySelector("#script");
 let limit = 0;
 setInterval(() => {
-    if (limit == 30) {
-       clearInterval(); 
-    }else{
-        limit += 1;
-        script.innerHTML = counter + "%";
-    }
+  if (limit == 30) {
+    clearInterval();
+  } else {
+    limit += 1;
+    script.innerHTML = counter + "%";
+  }
 }, 1000);
 navSlide();
+
+const formAll = document.querySelector("#form");
+const userName = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const button = document.querySelector("#btn");
+
+// Error handling
+const errorMgs = document.querySelector("#error");
+const errorPws = document.querySelector("#pswE");
+
+// Funtionality check
+const validate = () => {
+  if (userName.value === "") {
+    document.querySelector("#error").innerHTML = "Please enter username";
+    errorMgs.style.visibility = "visible";
+  }else{
+    errorMgs.style.visibility ="hidden";
+  }
+
+  if (passwordInput.value === "") {
+    document.querySelector("#pswE").innerHTML = "Please enter password";
+    errorPws.style.visibility = "visible";
+  }else{
+    errorPws.style.visibility = "hidden";
+    
+  }
+};
+
+
+
+button.addEventListener("click", (event) => { 
+  if (userName.value == "" || passwordInput.value == "") {
+    alert("Please enter your credentials");
+  }else{
+    localStorage.setItem("username", JSON.stringify(userName.value));
+    localStorage.setItem("password", JSON.stringify(passwordInput.value));
+    alert(JSON.stringify(userName.value + ' please access the login page to confirm details'));
+    window.location.href ="http://127.0.0.1:5500/login.html";
+  }
+  validate();
+ 
+});
---
+++ login.js
@@ -0,0 +1,56 @@
+const form = document.querySelector('form');
+const userName = document.querySelector("#userName");
+const passwordInput = document.querySelector("#password");
+const buttonType = document.querySelector("#btn");
+
+// Error messages of the form 
+const errorMessages = document.querySelector("#error");
+const passwordMsg = document.querySelector("#errorP"); 
+const notMatchMsg = document.querySelector("#notmatch");
+const notSame = document.querySelector("#notsame");
+
+// Declaring localStorage properties "targeting"
+const regData = JSON.parse(localStorage.getItem("username"));
+const regPsw = JSON.parse(localStorage.getItem("password"));
+
+
+const validateAll = () => {
+    if (username.value === "" ) {
+    errorMessages.innerHTML = "Please enter a username";
+    errorMessages.style.visibility = "visible";
+    notMatchMsg.style.visibility = "hidden";
+    }
+    else if (username.value !== regData) {
+        notMatchMsg.innerHTML = "Password does not match";
+        notMatchMsg.style.visibility = "visible";
+        errorMessages.style.visibility = "hidden";
+    }else if (username.value === regData) {
+        errorMessages.style.visibility = "hidden";
+    }
+
+    if (passwordInput.value === "") {
+        passwordMsg.innerHTML = "Please enter a password";
+        passwordMsg.style.visibility = "visible";
+        notSame.style.visibility = "hidden";
+    }else if (passwordInput.value !== regPsw) {
+        notSame.innerHTML = "Password does not match";
+        notSame.style.visibility = "visible";
+        passwordMsg.style.visibility = "hidden";
+    }else if (passwordInput.value === regPsw) {
+        notSame.style.visibility = "hidden";
+        passwordMsg.style.visibility = "hidden";
+    }
+}
+
+
+
+buttonType.addEventListener("click",(event) => {
+    if (!username.value.match(regData) ||  !passwordInput.value.match(regPsw)) {
+        alert("Incorrect details!");
+    }else{
+        alert("Welcome!");
+        location.reload();
+    }
+    validateAll();
+
+})
